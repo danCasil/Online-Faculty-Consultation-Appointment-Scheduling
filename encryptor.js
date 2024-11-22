1050a7f0049e471e1af2e4b351974c3ef78875b7
danypro5555 <css10388708002x@gmail.com>
2024-11-22 15:21:15 +0800

    delete and dowload

--- encryptor.js
+++ encryptor.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcryptjs");
 /*const pool = require('./postdb');
 function queryDatabase(query, params = []) { 
   return new Promise((resolve, reject) => {
