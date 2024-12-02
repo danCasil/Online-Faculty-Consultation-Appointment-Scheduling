a6645a349504c31cadf1ddeb199b4e90c8c3ec84
danypro5555 <css10388708002x@gmail.com>
2024-12-02 19:29:43 +0800

    Update postdb.js

--- postdb.js
+++ postdb.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool =new Pool({
- connectionString: process.env.DATABASE_URL_TEST,
- //Online
- //ssl: {rejectUnauthorized: false}
- //Ofline
-     ssl:false
+ connectionString: process.env.DATABASE_URL_ONLINE,
+
+ ssl: {rejectUnauthorized: false}
+
+    // ssl:false
 });
 
 module.exports = pool;
